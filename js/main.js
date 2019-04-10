f45110cce66cdda46686e2437cdb63132f5c8365
Jamie <jamiehdavis96@gmail.com>
2019-04-10 21:50:22 +0100

    init

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 function clickTest() {
     var x = document.getElementById("hamburger-button");
+    var y = document.getElementById("disquis");
+    console.log(y);
     if(x.className === "hamburger"){
         x.className += " show";
+        y.className += " show";
     } else {
         x.className = "hamburger";
+        y.className = "disquis";
     }
 }
