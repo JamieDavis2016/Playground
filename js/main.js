838073e01192c030b241368ffd92e1523c25ae44
JamieDavis2016 <jamiehdavis96@gmail.com>
2019-08-28 22:29:50 +0100

    slider carousel
    Pure javascript carousel

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 function clickTest() {
     var x = document.getElementById("hamburger-button");
     var y = document.getElementById("disquis");
-    console.log(y);
-    if(x.className === "hamburger"){
+    if (x.className === "hamburger") {
         x.className += " show";
         y.className += " show";
     } else {
@@ -11,11 +10,24 @@ function clickTest() {
     }
 }
 
+function stuff() {
+    var element = document.getElementById("carousel-example-div");
+    var imgElement = element.children;
+
+    for (var i = 0; i < imgElement.length; i++) {
+        imgElement[i].removeAttribute("hidden");
+    }
+}
+
+var slideIndex = 0;
 function carouselTimer() {
-    console.log("imgElement");
-    setTimeout(function () {
-        var element = document.getElementById("carousel-example-div");
-        var imgElement = element.children();
-        console.log(imgElement);
-    }, 2000);
+    var element = document.getElementById("carousel-example-div");
+    var imgElement = element.children;
+    for (var i = 0; i < imgElement.length; i++) {
+        imgElement[i].setAttribute("hidden", "hidden");
+    }
+    slideIndex++;
+    if(slideIndex > imgElement.length) { slideIndex = 1; }
+    imgElement[slideIndex-1].removeAttribute("hidden");
+    setTimeout(carouselTimer, 2000);
 }
