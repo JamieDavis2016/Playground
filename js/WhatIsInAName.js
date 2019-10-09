c0a5bb851449dabcf7beb5ef1a12f2316dd64272
JamieDavis2016 <jamiehdavis96@gmail.com>
2019-10-09 18:19:20 +0100

    FCC

--- js/WhatIsInAName.js
+++ js/WhatIsInAName.js
@@ -1,22 +1,19 @@
 function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
-  // Only change code below this line    
-  var srcKeys = Object.keys(source);
-
-  console.log(srcKeys);
-// obj.hasOwnProperty(srcKeys[i]) || 
-// obj[srcKeys[i]] !== source[srcKeys[i]]
-  collection.filter(item => {
-    for(var i = 0; i < srcKeys.length; i++) {
+  // Only change code below this line
+for(var keyd in source) {
+}
+  for(var key in collection) {
+      if(source[keyd] === collection[key][keyd]) {
+        arr.push(collection[key]);
+      } 
     }
-    if(item.hasOwnProperty(source)) {
-      arr.push(item);
-    }
-  });
-
+   console.log(arr);
   // Only change code above this line
-  return arr;
+  return (arr);
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+// whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })
