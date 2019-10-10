28cef11797c9fe299a3d4d327603bac2b0477dc5
JamieDavis2016 <jamiehdavis96@gmail.com>
2019-10-10 23:33:43 +0100

    FCC

--- js/WhatIsInAName.js
+++ js/WhatIsInAName.js
@@ -1,19 +1,49 @@
 function whatIsInAName(collection, source) {
+  // "What's in a name? that which we call a rose
+  // By any other name would smell as sweet.”
+  // -- by William Shakespeare, Romeo and Juliet
+  var srcKeys = Object.keys(source);
+  // filter the collection
+  return collection.filter(function (obj) {
+    for(var i = 0; i < srcKeys.length; i++) {
+      // for(var a in obj) {
+      //   console.log(a);
+      // }
+      // console.log(!obj.hasOwnProperty(srcKeys[i]));
+       console.log(obj[srcKeys[i]]);
+       console.log(obj[srcKeys[i]]);
+      if(!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
+// whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: "a" }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })
+
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })
+
+function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
   // Only change code below this line
-for(var keyd in source) {
-}
-  for(var key in collection) {
-      if(source[keyd] === collection[key][keyd]) {
-        arr.push(collection[key]);
-      } 
-    }
-   console.log(arr);
-  // Only change code above this line
-  return (arr);
-}
+  var srcKeys = Object.keys(source);
 
-// whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+  collection.forEach(obj => {
+    for(var i = 0; i < srcKeys.length; i++) {
+      if(obj.hasOwnProperty(srcKeys[i]) && obj[srcKeys[i]] === source[srcKeys[i]]) {
+      arr.push(obj);
+      }
+    }
+  });
+
+  console.log(arr);
+  // Only change code above this line
+  return arr;
+}
 
 whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })
+
+
+// whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
