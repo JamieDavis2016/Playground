182d45254410ecec84720a207e0adbd5fd93872e
Jamie <jamiehdavis96@gmail.com>
2019-10-11 22:14:26 +0100

    FCC I hve no idea what I'm doing

--- js/WhatIsInAName.js
+++ js/WhatIsInAName.js
@@ -47,3 +47,39 @@ whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "
 
 
 // whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+
+
+
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  
+  //match keys
+  // for(var i = 0; i < collection.length; i++) {
+  // for(var x in source) {
+  //     if(collection[i].hasOwnProperty(x) && source[x] == collection[i][x]) {
+  //         arr.push(collection[i]);
+  //     }
+  //   }
+  // }
+
+  collection.filter((obj) => {
+    for(var x in source)
+      if(obj.hasOwnProperty(x) && source[x] == obj[i]) {
+        console.log("a");
+        arr.push(obj);
+      }
+  }); 
+  
+  console.log(arr);
+  // Only change code above this line
+  return arr;
+}
+
+// whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })
