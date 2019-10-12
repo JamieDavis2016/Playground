622e2adf0fd72c29984645ccf4f2ecd2784d6d42
Jamie <jamiehdavis96@gmail.com>
2019-10-12 18:40:46 +0100

    ....

--- js/WhatIsInAName.js
+++ js/WhatIsInAName.js
@@ -83,3 +83,54 @@ function whatIsInAName(collection, source) {
 // whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
 
 whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })
+
+
+
+
+
+
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  
+  //match keys
+  // for(var i = 0; i < collection.length; i++) {
+  // for(var x in source) {
+  //     if(collection[i].hasOwnProperty(x) && source[x] == collection[i][x]) {
+  //         arr.push(collection[i]);
+  //     }
+  //   }
+  // }
+
+var keys = Object.keys(source);
+
+  collection.forEach((obj) => {
+    for(var i = 0; i < keys.length; i++)
+    {
+      var prop = Object.getOwnPropertyNames(obj);
+      if(prop.length >= keys.length) {
+        if(!obj.hasOwnProperty(keys[i]) || obj[keys[i]] !== source[keys[i]]) {
+          return;
+        }
+          arr.push(obj);
+          return;
+      }
+    }
+  });  
+  
+  console.log(arr);
+  console.log();
+  
+  // Only change code above this line
+  return arr;
+}
+
+// whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+// whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })
+
+//whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 })
+
+ whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3})
